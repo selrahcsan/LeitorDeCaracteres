ed0060b37e45042c6bcae942d7606943264998a5
selrahcsan <charlesstna@proton.me>
2025-02-04 21:27:57 -0300

    Adiconado o código do main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include <string.h>
+
+int main(void){
+  int contador;
+  char minhaFrase[100]; //limita ao tamanho de 100 caracteres o vetor
+
+  printf("Digite a sua Frase");
+  scanf  ("%99[^\n]", minhaFrase);
+
+  for (contador = 0; contador < strlen(minhaFrase); contador++){
+    printf("%d->[%c]->" , contador, minhaFrase[contador]);
+  }
+  printf("\n");
+}
